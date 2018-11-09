4281cb9c630f884990ef907a98f173b8d2079623
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-11-09 17:14:55 +0200

    DriveControl: Use CommandQueue

--- include/gui/DriveControl.hpp
+++ include/gui/DriveControl.hpp
@@ -2,17 +2,19 @@
 #define ROBOKASIV2_HOST_DRIVECONTROL_HPP
 
 #include "hwio/SerialProto.hpp"
+#include "hwio/CommandQueue.hpp"
 
 
 namespace gui {
 
     class DriveControl {
     public:
-        DriveControl(hwio::SerialProto& serialProto);
+        DriveControl(hwio::SerialProto& serialProto, hwio::CommandQueue& cq);
         ~DriveControl() = default;
         void render();
     private:
         hwio::SerialProto&  _serialProto;
+        hwio::CommandQueue& _commandQueue;
         hwio::Command       _command;
         bool                _initialControlsSet;
         bool                _contiguousMode;
