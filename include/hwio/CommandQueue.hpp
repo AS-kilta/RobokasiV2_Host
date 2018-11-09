1ec538a168467ff14d0511c25ffa3feb3f765ec0
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-11-09 17:14:23 +0200

    hwio: Add CommandQueue

---
+++ include/hwio/CommandQueue.hpp
@@ -0,0 +1,32 @@
+#ifndef ROBOKASIV2_HOST_COMMANDQUEUE_HPP
+#define ROBOKASIV2_HOST_COMMANDQUEUE_HPP
+
+
+#include "hwio/SerialProto.hpp"
+
+#include <deque>
+#include <mutex>
+#include <vector>
+
+
+namespace hwio {
+
+    class CommandQueue {
+    public:
+        CommandQueue(SerialProto& serial);
+        ~CommandQueue() = default;
+        void bufferNotify(size_t size, size_t capacity);
+        void addCommands(std::vector<Command>& cmds);
+        void addCommand(const Command& cmds);
+        size_t size();
+        unsigned duration();
+    private:
+        SerialProto& _serial;
+        /* _queue is accessed from another thread via bufferNotify() */
+        std::mutex _queueLock;
+        std::deque<Command> _queue;
+    };
+
+}
+
+#endif
---
+++ src/hwio/CommandQueue.cpp
@@ -0,0 +1,51 @@
+#include "hwio/CommandQueue.hpp"
+
+#include <mutex>
+
+
+using namespace hwio;
+
+
+CommandQueue::CommandQueue(SerialProto& serial) :
+    _serial(serial)
+{
+}
+
+void CommandQueue::bufferNotify(size_t size, size_t capacity)
+{
+    std::lock_guard<std::mutex> guard(_queueLock);
+    for (size_t i = size; i < capacity && !_queue.empty(); ++i) {
+        _serial.sendCommand(_queue.front());
+        _queue.pop_front();
+   }
+}
+
+void CommandQueue::addCommands(std::vector<Command>& cmds)
+{
+    std::lock_guard<std::mutex> guard(_queueLock);
+    std::move(begin(cmds), end(cmds), back_inserter(_queue));
+}
+
+void CommandQueue::addCommand(const Command& cmd)
+{
+    std::lock_guard<std::mutex> guard(_queueLock);
+    _queue.push_back(cmd);
+}
+
+size_t CommandQueue::size()
+{
+    std::lock_guard<std::mutex> guard(_queueLock);
+    return _queue.size();
+}
+
+unsigned CommandQueue::duration()
+{
+    std::lock_guard<std::mutex> guard(_queueLock);
+
+    unsigned t = 0;
+
+    for (const Command& cmd : _queue)
+        t += cmd.dt;
+
+    return t;
+}
