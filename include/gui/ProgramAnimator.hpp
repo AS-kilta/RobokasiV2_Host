c914c6976d2d3eeacd9e38da17fff4ca14bb32b1
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-11-15 16:48:24 +0200

    gui: Implement ProgramAnimator
    Program animator animates whole programs and drive the StepAnimator by
    feeding it the program steps in sequence.

---
+++ include/gui/ProgramAnimator.hpp
@@ -0,0 +1,35 @@
+#ifndef ROBOKASIV2_GUI_PROGRAMANIMATOR_HPP
+#define ROBOKASIV2_GUI_PROGRAMANIMATOR_HPP
+
+#include "kinematics/Program.hpp"
+#include "gui/VisualizerConfig.hpp"
+#include "gui/StepAnimator.hpp"
+
+namespace gui {
+    class ProgramAnimator {
+    public:
+        ProgramAnimator(const kin::Program& prog,
+                        VisualizerConfig& visualizerConfig);
+        void render(uint32_t dt);
+    private:
+        const kin::Program& _program;
+        VisualizerConfig& _visualizerConfig;
+        enum AnimationState {
+            Stop,
+            Run,
+            Pause,
+        };
+        int _animationState = AnimationState::Stop;
+        size_t _curStepIdx;
+        size_t _stepFrameIdx;
+        uint32_t _stepFrameDtAccum;
+        kin::Puma560 _startPose;
+        kin::Puma560 _prevPose;
+        kin::Puma560 _nextPose;
+        std::array<float, 6> _angles = {0};
+        std::array<float, 6> _visualizer();
+        StepAnimator _stepAnimator;
+    };
+};
+
+#endif
