944147caedf47db7050c78d89e9d5bedb5628f1a
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-11-16 02:07:01 +0200

    LinearDrive: Implement single frame mode

--- include/kinematics/LinearDrive.hpp
+++ include/kinematics/LinearDrive.hpp
@@ -8,8 +8,20 @@ namespace kin {
         LinearDrive(std::string name);
         std::vector<Puma560StepFrame> generate(const Puma560& poseA,
                                                const Puma560& poseB);
+        /*
+         * The linear drive program step can either generate multiple steps with
+         * constant dt between each frame or it can create a single frame with
+         * a large dt equaling the total transition time. The latter approach
+         * relies on the linear interpolation of frames further down the
+         * pipeline. The single frame mode should be used as the first step in
+         * a program to slowly drive robot to the starting position.
+         */
+        bool multiFrame = false;
+        /* Multi frame mode variables */
         float vel = 10.0f; /* deg / s */
         int dt = 100; /* ms */
+        /* Single frame mode variables */
+        int transitionTime = 5000; /* ms */
     };
 }
 
--- src/kinematics/LinearDrive.cpp
+++ src/kinematics/LinearDrive.cpp
@@ -14,32 +14,36 @@ std::vector<Puma560StepFrame> LinearDrive::generate(const Puma560& poseA,
 {
     std::vector<Puma560StepFrame> frames;
 
-    std::array<float, 6> deltas;
-    std::array<float, 6> absDeltas;
+    if (multiFrame) {
+        std::array<float, 6> deltas;
+        std::array<float, 6> absDeltas;
 
-    for (size_t i = 0; i < 6; ++i) {
-        deltas[i] = poseB.getJointAngle(i) - poseA.getJointAngle(i);
-        absDeltas[i] = fabs(deltas[i]);
-    }
+        for (size_t i = 0; i < 6; ++i) {
+            deltas[i] = poseB.getJointAngle(i) - poseA.getJointAngle(i);
+            absDeltas[i] = fabs(deltas[i]);
+        }
 
-    float maxDelta = *std::max_element(begin(absDeltas), end(absDeltas));
+        float maxDelta = *std::max_element(begin(absDeltas), end(absDeltas));
 
-    if (maxDelta == 0.0f)
-        return frames;
+        if (maxDelta == 0.0f)
+            return frames;
 
-    float velRadPerMs = vel * (PI / 180) * 0.001;
-    int t = maxDelta / velRadPerMs;
-    size_t numFrames = t / dt;
+        float velRadPerMs = vel * (PI / 180) * 0.001;
+        int t = maxDelta / velRadPerMs;
+        size_t numFrames = t / dt;
 
-    for (size_t i = 0; i < 6; ++i)
-        deltas[i] *= (float)dt / t;
+        for (size_t i = 0; i < 6; ++i)
+            deltas[i] *= (float)dt / t;
 
-    for (size_t i = 0; i < numFrames; ++i) {
-        Puma560StepFrame frame;
-        for (size_t j = 0; j < 6; ++j)
-            frame.setJointAngle(j, poseA.getJointAngle(j) + i * deltas[j]);
-        frame.dt = dt;
-        frames.push_back(frame);
+        for (size_t i = 0; i < numFrames; ++i) {
+            Puma560StepFrame frame;
+            for (size_t j = 0; j < 6; ++j)
+                frame.setJointAngle(j, poseA.getJointAngle(j) + i * deltas[j]);
+            frame.dt = dt;
+            frames.push_back(frame);
+        }
+    } else {
+        frames.emplace_back(poseB, transitionTime);
     }
 
     return frames;
